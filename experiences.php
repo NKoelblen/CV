b201c4b410265191a8cc66dfcfaea1d8707e275a
Noémie Koelblen <noemie.koelblen@icloud.com>
2024-09-05 09:15:09 +0200

    assistance informatique

--- experiences.php
+++ experiences.php
@@ -1,72 +1,126 @@
 <?php
 $experiences = [
     [
-        'years' => '2024',
-        'title' => 'Onoko photo',
-        'link' => 'onoko.dev/projects/onoko-photo',
-        'description' => 'Création from scratch du blog d\'un photographe amateur',
+        "techno" => "_ en PHP :",
+        'stack' => [
+            'MySQL',
+            'Bootstrap',
+            'Sass',
+            'JQuery',
+            'Leaflet JS'
+        ],
         'tasks' => [
-            'Développement <strong>Back-end</strong> & <strong>Front-end</strong> { <i>PHP</i> | <i>Bootstrap</i> | <i>Leaflet JS</i> }',
             'Architecture <strong>MVC</strong>',
-            'Optimisation de <strong>Base de Données</strong> { <i>MySQL</i> }',
-            'Back-office & système d\'<strong>authentification</strong>',
-            'Gestion de <strong>permissions</strong>'
+            'Back-office & système d\'<strong>Authentification</strong>',
+            'Gestion de <strong>permissions</strong>',
+            'Collections récursives { Albums | Catégories | Lieux }',
+            'Filtres & Carte du monde interractive',
+        ],
+        'projects' => [
+            [
+                'years' => '2024',
+                'title' => 'Onoko photo',
+                'link' => 'onoko.dev/projects/onoko-photo',
+                'description' => 'Création from scratch du blog d\'un photographe amateur',
+            ]
         ]
     ],
+
     [
-        'years' => '2024',
-        'title' => 'Onoko',
-        'link' => 'onoko.dev',
-        'description' => 'Création de mon Portfolio',
+        "techno" => "_ avec Symfony :",
+        'stack' => [
+            'Doctrine',
+            'Twig',
+            'Bootstrap',
+            'Sass',
+            'JQuery',
+            'Leaflet JS'
+        ],
         'tasks' => [
-            '<strong>Design</strong>',
-            'Développement <strong>Front-end</strong> & <strong>Back-end</strong> { <i>Wordpress</i> | <i>Sass</i> | <i>Javascript</i> }',
+            'Utilisation de l\'<strong>API Spotify</strong>',
+            '<strong>Web scraping</strong>',
+            'Développement d\'<strong>API REST</strong>',
+            'Back-office & système d\'<strong>Authentification</strong>',
+        ],
+        'projects' => [
+            [
+                'years' => '2024',
+                'title' => 'Mmmusic',
+                'link' => 'onoko.dev/projects/mmmusic',
+                'description' => 'Création dynamique de playlists Spotify',
+            ]
+        ]
+    ],
+
+    [
+        "techno" => "_ sur Wordpress :",
+        'stack' => [
+            'Bootstrap',
+            'Sass',
+            'JQuery',
+            'Leaflet JS'
+        ],
+        'tasks' => [
+            '<strong>Design</strong>, Maquettes & PAO',
             'Création de <strong>Thème</strong> & <strong>Plugin</strong>',
-            '<strong>Post Types</strong> & <strong>Metaboxes</strong> personnalisés',
-            'Gestion d\'<strong>Hébergement</strong> { <i>OVH</i> }'
-        ],
-    ],
-    [
-        'years' => '2024',
-        'title' => 'Mmmusic',
-        'link' => 'onoko.dev/projects/mmmusic',
-        'description' => 'Création dynamique de playlists Spotify',
-        'tasks' => [
-            'Développement <strong>Back-end</strong> { <i>Symfony</i> | <i>Twig</i> }',
-            'Développement d\'<strong>API REST</strong> { <i>Postman</i> }',
-            'Optimisation de <strong>Base de Données</strong> { <i>MySQL</i> | <i>Doctrine</i> }',
-            '<strong>Back-office</strong> & Authentification',
-        ],
-    ],
-    [
-        'years' => '2024',
-        'title' => 'Théâtre du Lyon',
-        'link' => 'onoko.dev/projects/theatre-du-lyon',
-        'description' => 'Création from scratch du site Web d\'une Compagnie de théâtre',
-        'tasks' => [
-            '<strong>Revue de code</strong>',
-            'Développement <strong>Front-end</strong> & <strong>Back-end</strong> { <i>Wordpress</i> | <i>Bootstrap</i> }',
-            'Création de <strong>Thème</strong>',
-            '<strong>Post Types</strong> & <strong>Metaboxes</strong> personnalisés',
-            'Traitement de <strong>Tickets</strong>',
-        ],
-    ],
-    [
-        'years' => '2023-2024',
-        'title' => 'Azoth',
-        'link' => 'onoko.dev/projects/azoth-v-2',
-        'description' => 'Refonte du site Web d\'une association d\'Arts martiaux',
-        'tasks' => [
-            '<strong>Design</strong>, Maquettes & PAO { <i>Figma</i> | <i>Illustrator</i> }',
-            '<strong>Revue de code</strong>',
-            'Développement <strong>Front-end</strong> & <strong>Back-end</strong> { <i>Wordpress</i> | <i>Bootstrap</i> | <i>JQuery</i> | <i>Leaflet JS</i> }',
-            'Création de <strong>Thème</strong>',
-            '<strong>Post Types</strong> & <strong>Metaboxes</strong> personnalisés',
+            '<strong>Post Types</strong>, <strong>Metaboxes</strong> & <strong>Taxonomies</strong> personnalisés',
             '<strong>Rôles</strong> personnalisés',
-            'Tâches <strong>CRON</strong>',
-            '<strong>SEO</strong> & <strong>Analyse d\'audience</strong> { <i>Yoast SEO</i> | <i>Google Analytics</i> }',
-            'Gestion d\'<strong>Hébergement</strong> { <i>O2Switch</i> }',
-            'Traitement de <strong>Tickets</strong>',
+            'Mails d\'informations automatisés : Formulaire d\'abonnement & Tâche <strong>CRON</strong>',
+            'Filtres & Carte du monde interractive'
         ],
+        'projects' => [
+            [
+                'years' => '2022-2024',
+                'title' => 'Azoth',
+                'link' => 'onoko.dev/projects/azoth-v-2',
+                'description' => 'Refonte du site Web d\'une association d\'Arts martiaux',
+            ],
+            [
+                'years' => '2023-2024',
+                'title' => 'Théâtre du Lyon',
+                'link' => 'onoko.dev/projects/theatre-du-lyon',
+                'description' => 'Création from scratch du site Web d\'une Compagnie de théâtre',
+            ],
+            [
+                'years' => '2024',
+                'title' => 'MotaPhoto',
+                'description' => 'Projet de formation : Intégration du blog d\'un photographe professionnel',
+            ],
+            [
+                'years' => '2024',
+                'title' => 'Onoko',
+                'link' => 'onoko.dev',
+                'description' => 'Création de mon Portfolio',
+            ],
+        ]
+
+    ],
+    [
+        "techno" => "_ en assistance informatique",
+        'stack' => [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "PHP",
+            "SQL",
+            "Access",
+            "VBA",
+        ],
+        'tasks' => [
+            'Administration d\'un site internet dynamique → <a href="https://web.archive.org/web/20191227095649/http://www.pacei-formation.org/">www.pacei-formation.org</a>',
+            'Conception et administration de Bases de Données Relationnelle { Access }',
+            'Configuation d\'ERP { EBP | Sage X3 }',
+            'Web scrapping',
+            'Assistance Hardware & Software',
+            'PAO & DAO { Adobe Illustrator & Indesign | QuarkXpress }',
+            'Animation de formations bureautiques'
+        ],
+        'projects' => [
+            [
+                'years' => '2013-2023',
+                'title' => 'SNCF, ISIMA, POLYTECH\', PACEI, CIREME, Phytosynthèse',
+                'description' => 'Chargée de Gestion des Archive |  Secrétaire polyvalente | Assistante comptable & informatique'
+            ],
+        ]
     ]
 ];
